94cb5a19e1a1c0d81bce140444cb5f5aff139c54
amchawla <amit.chawla@tatacommunications.com>
2019-12-22 18:11:57 +0530

    Updates

---
+++ precache-manifest.9b3a1630a3badf5913ca5d30d7d7df61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0df2de48fa9a0ff8c14cca143e1bf94",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d5812955f5cc3e4333e8",
+    "url": "/tictactoe/static/css/main.7be0a0ea.chunk.css"
+  },
+  {
+    "revision": "916e93b1bca9ee119b18",
+    "url": "/tictactoe/static/js/2.a318b497.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe/static/js/2.a318b497.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5812955f5cc3e4333e8",
+    "url": "/tictactoe/static/js/main.45f79be4.chunk.js"
+  },
+  {
+    "revision": "d5cef6b0326e4be2e1fe",
+    "url": "/tictactoe/static/js/runtime-main.d953a2bb.js"
+  }
+]);
